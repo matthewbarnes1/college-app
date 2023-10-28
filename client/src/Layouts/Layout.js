9c97decac9440000837507b603f7bbce40ec23fa
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-10-28 17:09:33 -0400

    added componenets and basic page layout

---
+++ client/src/Layouts/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+const Layout =({children}) =>{
+    return(
+        <>
+        <div>
+        <Header/>
+        </div>
+        <main>{children}</main>
+        <Footer />
+        </>
+
+    )
+}
+
+export default Layout;
---
+++ client/src/components/navbar.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <h1>Navbar</h1>
+        </div>
+
+    )
+}
+
+export default Navbar;
+
