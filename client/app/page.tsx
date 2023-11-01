239ac79ed30614b8a5b191564ec3ee60a6573037
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-11-01 19:24:47 -0400

    added some more meta deets for homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,23 +1,50 @@
 
 export default function Home() {
   return (
+    <>
 <main>
-  <h1>THis is the homepage</h1>
-  <div>
+  <h1>This is the homepage</h1>
+
+  <div className="hero">
+    <h2>This is the hero card that will grab users attention (some graphic etc) </h2>
+    <p>
+      Short catch-phrase eye grabbing text / info
+    </p>
+  </div>
+
+  <div className="application description">
     <h2>Description</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat porttitor magna, ac 
       tristique orci convallis ut. Phasellus vel ligula ut ex vulputate blandit. Aliquam erat 
       volutpat. Aenean eros enim, commodo vel sapien eu, sodales maximus felis. Nullam cursus 
       ullamcorper nisl a tempus. Mauris ultricies, ipsum non iaculis tristique, quam orci semper 
       leo, eget lobortis velit metus ut nunc. Quisque sit amet tortor in dolor venenatis ullamcorper. 
-      Morbi lectus nulla, congue ut rutrum at, congue vitae neque. Donec a nunc faucibus, blandit risus 
-      eget, accumsan erat. Proin sed cursus sapien. Donec tincidunt 
-      sagittis odio, non pulvinar ligula finibus vel. Vestibulum ante ipsum primis in faucibus 
-      orci luctus et ultrices posuere cubilia curae; Quisque ipsum odio, dictum 
-      euismod cursus eu, maximus id purus. Duis bibendum cursus nisi. Mauris fermentum quam mi, 
-      fermentum egestas massa scelerisque eget. Donec rutrum hendrerit nunc ac luctus.</p>
+      </p>
   </div>
+
+  <div className="events description">
+    <h2>Events</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat porttitor magna, ac 
+      tristique orci convallis ut. Phasellus vel ligula ut ex vulputate blandit. Aliquam erat 
+      volutpat. Aenean eros enim, commodo vel sapien eu, sodales maximus felis. Nullam cursus 
+      ullamcorper nisl a tempus. Mauris ultricies, ipsum non iaculis tristique, quam orci semper 
+      leo, eget lobortis velit metus ut nunc. Quisque sit amet tortor in dolor venenatis ullamcorper. 
+   </p>
+  </div>
+
+  <div className="events description">
+    <h2>Events</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat porttitor magna, ac 
+      tristique orci convallis ut. Phasellus vel ligula ut ex vulputate blandit. Aliquam erat 
+      volutpat. Aenean eros enim, commodo vel sapien eu, sodales maximus felis. Nullam cursus 
+      ullamcorper nisl a tempus. Mauris ultricies, ipsum non iaculis tristique, quam orci semper 
+      leo, eget lobortis velit metus ut nunc. Quisque sit amet tortor in dolor venenatis ullamcorper. 
+   </p>
+  </div>
+  <carosel
 </main>
+<
+</>
 
   )
 }
